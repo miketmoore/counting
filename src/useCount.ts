ff16b41e292cb6151a87b47305dcca4ce653ff32
Michael Moore <mike@miketmoore.com>
2021-08-17 18:54:08 -0400

    get initial count via get api

--- src/useCount.ts
+++ src/useCount.ts
@@ -1,17 +1,31 @@
 import { useCallback, useDebugValue, useEffect, useState } from "react";
 import countapi from "countapi-js";
 
+const apiKey = "***REMOVED***";
+
 export const useCount = () => {
   const [requestEnabled, setRequestEnabled] = useState(false);
   const [count, setCount] = useState<number | null>(null);
   const [error, setError] = useState<Error | null>(null);
 
+  const getInitialCount = useCallback(async () => {
+    try {
+      const { value } = await countapi.get(apiKey);
+      setCount(value);
+    } catch (error) {
+      setError(error);
+    }
+  }, [setError]);
+
+  useEffect(() => {
+    if (count == null) {
+      getInitialCount();
+    }
+  });
+
   const sendRequest = useCallback(async () => {
     try {
-      const { value } = await countapi.update(
-        "***REMOVED***",
-        0
-      );
+      const { value } = await countapi.update(apiKey, 0);
       setCount(value);
     } catch (error) {
       setError(new Error("The count request failed"));
