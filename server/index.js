47c3d1165e2e7841c75ac9c06e1bbe6ad27d0e7f
Michael Moore <mike@miketmoore.com>
2021-08-18 18:55:56 -0400

    remote unirest

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-const unirest = require("unirest");
 const countapi = require("countapi-js");
 
 const apiKey = "***REMOVED***";
