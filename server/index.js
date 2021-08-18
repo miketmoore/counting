1d39f7ef757599cd49d342e725689f72b4c6e855
Michael Moore <mike@miketmoore.com>
2021-08-18 16:13:51 -0400

    add express server

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+const unirest = require("unirest");
+const countapi = require("countapi-js");
+
+const apiKey = "***REMOVED***";
+const incrementAmount = 1;
+
+app.get("/api/count/get", (req, res) => {
+  countapi
+    .get(apiKey)
+    .then((response) => {
+      res.json({ count: response.value });
+    })
+    .catch((error) => {
+      res.json({ message: "Unable to get current count", error });
+    });
+});
+
+app.put("/api/count/update", (req, res) => {
+  countapi
+    .update(apiKey, incrementAmount)
+    .then((response) => {
+      res.json({ count: response.value });
+    })
+    .catch((error) => {
+      res.json({ message: "Unable to update count", error });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
